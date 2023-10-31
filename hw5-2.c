63461a76389a528eaed86c2976e0da071942ef20
chegnyu <146065063+chegnyu@users.noreply.github.com>
2023-10-31 14:27:30 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int min;
+    double cost;
+    scanf("%d",&min);
+    if (min<800){
+        cost = min*0.9;
+        printf("%0.1f",cost);
+        }
+        if(min>800 && min<1500){
+            cost = (min*0.9)*0.9;
+            printf("%0.1f",cost);
+            }
+        if (min>=1500){
+                cost = (min*0.9)*0.79;
+                printf("%0.1f",cost);
+                }
+
+            }
+
+
+
+
 
