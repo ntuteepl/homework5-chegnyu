420a297fef247470ce8af3da73bbe66c1939cbae
chegnyu <146065063+chegnyu@users.noreply.github.com>
2023-10-31 14:47:53 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int w,t;
+   double s;
+   scanf("%d%d",&w,&t);
+   if(w<=60){
+    s = w*t;
+   }
+   else if(w<=120){
+    s = w*1.33*t;
+   }
+   else {
+    s = w*1.66*t;
+   }
+   printf("%0.1f",s);
+}
 
